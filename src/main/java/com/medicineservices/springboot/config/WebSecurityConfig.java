c0e1caf36a737aca46b8f56f9abb7577d1f9958c
damirEDS <esenaevdamir123@gmail.com>
2023-10-19 13:45:13 +0600

    comm

--- src/main/java/com/medicineservices/springboot/config/WebSecurityConfig.java
+++ src/main/java/com/medicineservices/springboot/config/WebSecurityConfig.java
@@ -34,8 +34,8 @@ public class WebSecurityConfig {
     public UserDetailsService userDetailsService() {
         UserDetails user =
                 User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
+                        .username("u")
+                        .password("p")
                         .roles("USER")
                         .build();
 
