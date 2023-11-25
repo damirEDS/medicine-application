4b87dd52bace82f61b0db1e3f158fe0e9adee0b1
damirEDS <esenaevdamir123@gmail.com>
2023-11-25 15:30:26 +0600

    TranslationService

--- src/main/java/com/medicineservices/springboot/security/controllers/AuthController.java
+++ src/main/java/com/medicineservices/springboot/security/controllers/AuthController.java
@@ -1,7 +1,6 @@
 package com.medicineservices.springboot.security.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.medicineservices.springboot.security.dtos.JwtRequest;
 import com.medicineservices.springboot.security.dtos.RegistrationUserDto;
@@ -16,21 +15,10 @@ public class AuthController {
         this.authService = authService;
     }
 
-    @GetMapping("/auth")
-    public String showLoginForm(Model model) {
-        model.addAttribute("authRequest", new JwtRequest());
-        return "login"; // This will return the login2.html Thymeleaf template
-    }
 
     @PostMapping("/auth")
     public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
         return authService.createAuthToken(authRequest);
-    }
-
-    @GetMapping("/registration")
-    public String showRegistrationForm(Model model) {
-        model.addAttribute("registrationUserDto", new RegistrationUserDto());
-        return "register"; // This will return the register.html Thymeleaf template
     }
 
     @PostMapping("/registration")
---
+++ src/main/java/com/medicineservices/springboot/translations/TranslationService.java
@@ -0,0 +1,20 @@
+package com.medicineservices.springboot.translations;
+
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@Service
+public class TranslationService {
+
+    private final MessageSource messageSource;
+
+    public TranslationService(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    public String getTranslation(String messageId, Locale locale) {
+        return messageSource.getMessage(messageId, null, locale);
+    }
+}
